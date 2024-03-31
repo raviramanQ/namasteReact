c2c211a7b7161389401effc05e9d4ae5e6f50c22
Ravi <raviraman03111996@gmail.com>
2024-03-31 12:15:56 +0530

    inception complete

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let header = React.createElement("h1",{id:"my-jocker"},"hi vicky, how are you ?");
+let header = React.createElement(
+    
+    "h1",
+    {id:"my-jocker"},[
+        React.createElement("h2",{},
+        "hi Ram, how are you ?"
+        ),
+        React.createElement("h3",{},
+        "hi karim, how are you ?"
+        )
+    
+    ]
+    );
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
+
+// react element always return object @@@ above header returns object.
+
+// root.render replacing what is there inside root.
