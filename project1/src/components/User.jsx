cc574acd31a5f4bbe540f04a78d2d3c54eba4b8b
Ravi <raviraman03111996@gmail.com>
2024-05-25 21:46:09 +0530

    21 may

--- project1/src/components/User.jsx
+++ project1/src/components/User.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const User = ({name,location,contact}) =>{
  
      const [counter] = useState(0);
      const [counter2] = useState(2);
 
+     useEffect()
+     {
+          console.log('test1111');
+     }
+
 // console.log(props);
      return(
           <div className="user-card">
+               {console.log('omega')}
                <h1>Name: {name}</h1>
                <h2>Location: {location}</h2>
                <h3>Contact: {contact}</h3>
